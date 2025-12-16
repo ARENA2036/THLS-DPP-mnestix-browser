b90b3272a6bf343f3496f41b7a544665ccbf2a42
melanie.gail <melanie.gail@xitaso.com>
2025-12-16 11:13:25 +0100

    unused import removed

--- src/app/[locale]/templates/_components/blueprint-edit/edit-components/multiplicity/MultiplicityEditComponent.tsx
+++ src/app/[locale]/templates/_components/blueprint-edit/edit-components/multiplicity/MultiplicityEditComponent.tsx
@@ -5,7 +5,6 @@ import { BlueprintEditSectionHeading } from 'app/[locale]/templates/_components/
 import multiplicityDataJson from './multiplicity-data.json';
 import { MultiplicityEnum } from 'lib/enums/Multiplicity.enum';
 import { MultiplicityData } from 'lib/types/MultiplicityData';
-import { LockedTextField } from 'components/basics/LockedTextField';
 import { useTranslations } from 'next-intl';
 import { Qualifier, Submodel, SubmodelElementChoice } from 'lib/api/aas/models';
 
